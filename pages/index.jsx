733d172b9235faaf3f0cd4cb7ece931ec3cb03d0
lkarvec <lkarvec@gmail.com>
2024-02-28 22:58:38 -0600

    Move regen button to the corner

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import useBackground from '../lib/backgroundGenerator';
 import MainContent from '../views/MainContent';
 import React, { useMemo } from 'react';
-import { Container } from 'react-bootstrap';
 import Button from '../components/Button/Button';
+import classes from '../views/MainContent.module.scss';
 
 function App() {
     const { backgrounds, regenerate } = useBackground();
@@ -19,6 +19,7 @@ function App() {
 
             <Button
                 label="Regenerate colors"
+                className={classes.regenerate}
                 handleClick={() => {
                     regenerate();
                 }}
