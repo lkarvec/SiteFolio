8813665418341ce4c7e4f49d22c76ffad9123507
lkarvec <lkarvec@gmail.com>
2024-03-28 01:12:01 -0500

    fix linter errors causing deployment issues

--- lib/backgroundGenerator.tsx
+++ lib/backgroundGenerator.tsx
@@ -5,7 +5,7 @@ const palette = ['#855988', '#42b6c6', '#33a68c', '#446ba6', '#4b95f0', '#c16bc5
 // cache this to save memory
 const paletteLength = palette.length;
 
-export const generateBackground = (): string[] => {
+export const generateBackground = () => {
     return Array(5)
         .fill(null)
         .map(() => palette[Math.floor(Math.random() * paletteLength)])
