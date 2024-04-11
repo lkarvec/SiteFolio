5623b4207494bf8cb6be50e1b0f63ca7e5fa19c3
lkarvec <lkarvec@gmail.com>
2024-04-11 04:23:24 -0500

    Add Tiko's recommendation

--- views/WorkExperience/Booksmarts/Booksmarts.jsx
+++ views/WorkExperience/Booksmarts/Booksmarts.jsx
@@ -3,8 +3,10 @@ import { Col, Container, Row } from 'react-bootstrap';
 import ImageFlip from '../../../components/Images/ImageFlip/ImageFlip';
 import links from '../../../constants/imageUrls.module.scss';
 import classes from './Booksmarts.module.scss';
+import Card from '../../../components/Card/Card';
 
-/*
+/**
+ * React Component detailing all of my experience working on Book's site
  */
 
 const Booksmarts = () => {
@@ -16,6 +18,24 @@ const Booksmarts = () => {
                         <h2>Booksmarts LLC</h2>
                         <h3>Senior Software Developer</h3>
                         <h4>September 2020 - August 2022</h4>
+                    </Col>
+                    <Col md={12} lg={{ offset: 1, span: 6 }}>
+                        <Card>
+                            Leon&apos;s exceptional leadership, project management skills, and dedication were instrumental in the
+                            successful delivery of the Booksmarts website project. One of Leon&apos;s standout qualities was his keen eye
+                            for detail and his proactive approach to problem-solving. He not only ensured that the project stayed on
+                            schedule but also played a role in refining and enhancing the initial designs of the platform. His insightful
+                            contributions helped shape the website into a polished and user-friendly experience for Booksmarts&apos;
+                            audience. I wholeheartedly recommend him for any future endeavors and am grateful for the opportunity to have
+                            collaborated with such a talented individual.
+                            <ul>
+                                -{' '}
+                                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/samuel-waddell3/">
+                                    Tiko Nelson
+                                </a>
+                                , Lead Designer
+                            </ul>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
