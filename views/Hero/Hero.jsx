b5ebcaf05a8960f03e86450a1c91b5b91db02b56
lkarvec <lkarvec@gmail.com>
2024-04-11 10:30:03 -0500

    fix accessibility issue with headers

--- views/Hero/Hero.jsx
+++ views/Hero/Hero.jsx
@@ -20,16 +20,14 @@ const Hero = () => {
                             <h2>My name is Alexander Mains.</h2>
                         </Row>
                         <Row>
-                            <h4>
+                            <p>
                                 I&apos;m a recognized leader in website Software Engineering; delivering website overhauls and modernization
                                 projects with expertise in the React ecosystem, including Typescript, NextJS, AWS, and more.
-                            </h4>
-                            <h4 style={{ maxWidth: '1100px' }}>
+                            </p>
+                            <p style={{ maxWidth: '1100px' }}>
                                 I am currently on the market for new roles, as my current contract is finishing at the end of March 2024.
-                            </h4>
-                            <h4>
-                                Feel free to send me an email or a message on LinkedIn about new opportunities for React Web Development.
-                            </h4>
+                            </p>
+                            <p>Feel free to send me an email or a message on LinkedIn about new opportunities for React Web Development.</p>
                         </Row>
                         <Row>
                             <div className={classes.centered}>
