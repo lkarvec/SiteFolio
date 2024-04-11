1f8eaaf5774492da66ffb3d2f01717e9c734df15
lkarvec <lkarvec@gmail.com>
2024-04-11 09:55:14 -0500

    fix Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
+import Head from 'next/head';
 import '../styles/globals.scss';
 
 function Application({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title key="title">Feytouched Tech</title>
+                <meta
+                    key="description"
+                    name="description"
+                    content="A recognized leader in Frontend Software Engineering; delivering websites with expertise in the React ecosystem, Typescript, NextJS, AWS, and more."
+                />
+                <meta
+                    key="keywords"
+                    name="keywords"
+                    // eslint-disable-next-line max-len
+                    content="HTML, CSS, JavaScript, Frontend Developer, Senior Developer, Software Engineering, React, NextJS, Senior Software Engineer, Portfolio Website, Examples of Portfolio Websites, How to make a portfolio website, Software Developer for Hire, Frontend Developer for Hire"
+                />
+                <meta name="author" content="Alexander Mains" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default Application;
